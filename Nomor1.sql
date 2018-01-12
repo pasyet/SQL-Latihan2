e1ae29771a52bb26e87242cb7d29fa1740fe4c59
pasyet <farapasyetnabila@gmail.com>
2018-01-13 01:14:52 +0700

    Latihan 2

---
+++ Nomor1.sql
@@ -0,0 +1,20 @@
+select 
+	employee_id as "Kode Karyawan",
+
+    department_name as "Nama Department",
+
+	first_name || last_name as "Nama Lengkap",
+
+    to_char(salary, '99,999') as "Gaji Sebulan",
+
+    case when commission_pct IS NULL 	/*kasus dimana komisi = null*/
+    then 'Tidak punya komisi!'			/*maka Tidak punya komisi*/
+    else to_char((salary *commission_pct), '999,999') 	/*else salary*null(0)*/
+  	end as "Mendapatkan Komisi",
+
+  	to_char (salary + (salary * coalesce(commission_pct, 0)), '999,999') as "Gaji Terima"
+  	/*salary+(salary*/
+from 
+	employees,
+    departments
+
